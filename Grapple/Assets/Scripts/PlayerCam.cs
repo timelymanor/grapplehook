b2bbb371f7b5662a96225914b773fe228a507759
timelymanor <sigmamale1909@gmail.com>
2025-10-02 09:04:35 -0500

    Initial Commit

---
+++ Grapple/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // get mouse input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0); 
+    }
+}
---
+++ Grapple/Assets/Scripts/WallRunning.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    [Header("Wallrunning")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float maxWallRunTime;
+    private float wallRunTimer;
+
+    [Header("Input")]
+    private float horizontalInput;
+}
