e7f3f48d86d836057e54df7f54c97a8f6dd25c5e
timelymanor <sigmamale1909@gmail.com>
2025-10-22 08:58:35 -0500

    lineRenderer for grapplehook

--- Grapple/Assets/Scripts/Grappling.cs
+++ Grapple/Assets/Scripts/Grappling.cs
@@ -2,15 +2,83 @@ using UnityEngine;
 
 public class Grappling : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")] 
+    private PlayerMovementAdvanced pm;
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform gunTip;
+    [SerializeField] private LayerMask whatIsGrappleable;
+    [SerializeField] private LineRenderer lr;
+
+    [Header("Grappling")] 
+    [SerializeField] private float maxGrappleDistance;
+
+    [SerializeField] private float grappleDelayTime;
+    private Vector3 grapplePoint;
+
+    [Header("Cooldown")] 
+    [SerializeField] private float grapplingCd;
+    private float grapplingCdTimer;
+
+    [Header("Input")] 
+    [SerializeField] private KeyCode grappleKey = KeyCode.Mouse1;
+
+    private bool grappling;
+
+    private void Start()
+    {
+        pm = GetComponent<PlayerMovementAdvanced>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(grappleKey)) StartGrapple();
+
+        if (grapplingCdTimer > 0)
+        {
+            grapplingCdTimer -= Time.deltaTime;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(grappling)
+            lr.SetPosition(0, gunTip.position);
+    }
+
+    private void StartGrapple()
+    {
+        if (grapplingCdTimer > 0) return;
+        
+        grappling = true;
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, whatIsGrappleable))
+        {
+            grapplePoint = hit.point;
+            
+            Invoke(nameof(ExecuteGrapple), grappleDelayTime);
+        }
+        else
+        {
+            grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+            Invoke(nameof(StopGrapple), grappleDelayTime);
+        }
+        
+        lr.enabled = true;
+        lr.SetPosition(1, grapplePoint);
+    }
+
+    private void ExecuteGrapple()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StopGrapple()
     {
+        grappling = false;
         
+        grapplingCdTimer = grapplingCd;
+        
+        lr.enabled = false;
     }
 }
