8658f938fca041638d9aaf7edb824c804b95d591
timelymanor <sigmamale1909@gmail.com>
2026-03-10 09:29:54 -0500

    enemies have spread

--- Grapple/Assets/Scripts/Projectile.cs
+++ Grapple/Assets/Scripts/Projectile.cs
@@ -5,13 +5,10 @@ using Random = UnityEngine.Random;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float projectileSpread;
-    private float spread;
+    
+
     [SerializeField] private float damageAmount;
-    private Rigidbody rb;
-    private Transform player;
-    private PlayerMovementAdvanced pma;
+    
 
     
     public enum ProjectileType
@@ -22,14 +19,8 @@ public class Projectile : MonoBehaviour
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        pma = player.GetComponent<PlayerMovementAdvanced>();
-        spread = pma.getPlayerSpeed() / 4 + projectileSpread;
-        rb = GetComponent<Rigidbody>();
-        float randomX = Random.Range(-spread, spread);
-        float randomY = Random.Range(-spread, spread);
-        transform.Rotate(randomX, randomY, 0);
-        rb.linearVelocity = transform.forward * speed;
+       
+        
 
         Despawn();
     }
--- Grapple/Assets/Scripts/RangedEnemy.cs
+++ Grapple/Assets/Scripts/RangedEnemy.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class RangedEnemy : EnemyBase
 {
 
-    
+    [SerializeField] public float speed;
     [SerializeField] private GameObject projectile;
     [SerializeField] private int numOfProjectiles;
+    [SerializeField] private float projectileSpread;
+    private PlayerMovementAdvanced pma;
+    private Rigidbody rb;
+    
+    private float spread;
+
+    protected void Start()
+    {
+        pma = GameObject.Find("Player").GetComponent<PlayerMovementAdvanced>();
+    }
     
     protected override void AttackPlayer()
     {
@@ -24,7 +34,7 @@ public class RangedEnemy : EnemyBase
                 Quaternion rotation = Quaternion.LookRotation(direction);
                 for (int i = 0; i < numOfProjectiles; i++)
                 {
-                    Rigidbody rb = Instantiate(projectile, spawnPos, rotation).GetComponent<Rigidbody>();
+                    Fire();
                 }
             }
             else
@@ -32,5 +42,34 @@ public class RangedEnemy : EnemyBase
                 agent.SetDestination(player.position);
             }
         }
+    }
+    
+    void Fire()
+    {
+        // Safely get the player's Rigidbody to calculate their velocity
+        Rigidbody playerRb = player.GetComponent<Rigidbody>();
+        Vector3 playerVelocity = playerRb != null ? playerRb.linearVelocity : Vector3.zero;
+        
+        // Calculate time to target for leading the shot
+        float distance = Vector3.Distance(transform.position, player.position);
+        float timeToTarget = distance / speed;
+        
+        // Calculate the random spread based on player speed
+        spread = (pma.getPlayerSpeed() / 4f) + projectileSpread;
+        float randomX = Random.Range(-spread, spread);
+        float randomY = Random.Range(-spread, spread);
+
+        // Calculate lead position and base direction
+        Vector3 leadPosition = player.position + (playerVelocity * timeToTarget);
+        Vector3 shootDirection = (leadPosition - transform.position).normalized;
+
+        // Spawn the bullet, initially facing the perfect shoot direction
+        GameObject bullet = Instantiate(projectile, transform.position, Quaternion.LookRotation(shootDirection));
+        
+        // Apply the random spread rotation BEFORE applying velocity
+        bullet.transform.Rotate(randomX, randomY, 0);
+        
+        // Apply velocity along the bullet's NEW forward direction (which includes the spread)
+        bullet.GetComponent<Rigidbody>().linearVelocity = bullet.transform.forward * speed;
     }
 }
